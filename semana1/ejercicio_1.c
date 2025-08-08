7fb8f206c29fbc529e4fd5284db57651736eb9c7
anabusso01 <ap.bussosilva@ugto.mx>
2025-08-08 11:42:26 -0600

    Add files via upload

---
+++ semana1/ejercicio_1.c
@@ -0,0 +1,12 @@
+//08/08/2025
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+float x[5] = {1.0, 2.5, 3.0, 4.5, 5.0,};
+
+printf("x = %.2f\n", x[0]);
+printf("f(x) = x^2 = %.2f\n", x[0] * x[0]);
+printf("f(x) = log(x+1) = %.4f\n", log(x[0] +1 ));
+}
