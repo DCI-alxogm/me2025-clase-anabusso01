d851a7b5603f782adaab2d09bf5dc555cce1880a
anabusso01 <ap.bussosilva@ugto.mx>
2025-08-15 11:49:25 -0600

    Add files via upload

---
+++ Semana 2/ecuaciones.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float a, b, c;
+  float D, x1, x2, real, imag;
+  char repetir;
+
+  do{
+    printf("=== Raíces de ax^2+bx+c=0===\n");
+    printf("Introduce a: ");
+    scanf("%f", &a);
+    printf("Introduce b:");
+    scanf("%f", &b);
+    printf("Introduce c:");
+    scanf("%f", %c);
+
+    if (a==0) {
+      if (b==0){
+        if (c==0){
+          printf("infinitas soluciones (identidad 0=0)\n");
+        } else {
+          printf("Sin solución")
+        }
+        }
+      }
+    }
+  }
+}
